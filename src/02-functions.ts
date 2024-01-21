6335aa777f2e67c5149c262c0aec3fc87f7a9fbf
Seamus McCarthy <seamusmccarthy2508@gmail.com>
2024-01-21 12:39:01 +0000

    Function to update friends interests

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -84,3 +84,14 @@ function findFriends(
 
 // console.log(findFriends(friends, (friend) => friend.name.startsWith("Pa")));
 // console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, interest: string) {
+  if (friend.interests === undefined) {
+    friend.interests = [];
+  }
+  friend.interests.push(interest);
+  return friend.interests;
+}
+
+console.log(addInterest(friends[0], "Music"));
+console.log(addInterest(friends[0], "Politics"));
